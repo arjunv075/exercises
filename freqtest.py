cbaef3eb7bed758da7c9ac9452f6a82794305a96
arjunv075 <arjunv075@gmail.com>
2023-11-01 15:38:44 +0530

    1. freqtest program changes    changed program code to check the occurences of letters in a string

--- freqtest.py
+++ freqtest.py
@@ -1,24 +1,15 @@
+from importguard import freq
 import unittest
 
-def freq(s):
-    d={}
-    s=s.lower()
-    for i in s:
-        if i in d:
-            d[i]+=1
-        else:
-            d[i]=1
-    return d
 
 class TestFreq(unittest.TestCase):
-    def testIsFreq(self):
-        s = "malayalam"
-        self.assertTrue(freq(s))
         
-    
-    def testNullInput(self):
-        ret = freq("")
-        self.assertFalse(ret)
+    def test_dict(self):
+        d={'a': 2, 'b': 2, ' ': 1, '?': 1}
+        s="abba ?"
+        self.assertDictEqual(d,freq(s))
+
+
 
 
 if __name__ == "__main__":
