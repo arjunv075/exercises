03c5b426ce414a9301c4ed3cc68db0ee26730677
arjunv075 <arjunv075@gmail.com>
2023-11-01 17:57:05 +0530

    1. code green    test successfully passed including single operand.

--- postfixtest_1.py
+++ postfixtest_1.py
@@ -1,8 +1,8 @@
-from postfix import eval
+from postfix_1 import eval
 
 
 def test_singleoperand():
-    assert eval("4") ==""
+    assert eval("4") == 4
 
 def test_add():
     assert eval("26+") == 8
